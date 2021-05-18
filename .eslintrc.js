6a06c9a4befd4b05292702cf54b09719097e7e1c
Menachem Neiman <Nerevar123@gmail.com>
2021-05-18 19:31:01 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "prettier", "plugin:react/recommended"],
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    plugins: ["react", "prettier"],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+};

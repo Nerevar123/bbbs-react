2b4e5fda0b619468f181b541b67eb31a1af71b99
KateSmychok <70967958+KateSmychok@users.noreply.github.com>
2021-05-21 15:07:13 +0300

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import { Router, Route, Switch, useHistory } from "react-router-dom";
 import styles from "./App.module.scss";
 import Header from "../Header/Header";
 import Calendar from "../Calendar/Calendar";
+import Menu from "../Menu/Menu";
 import Main from "../Main/Main";
 
 function App() {
